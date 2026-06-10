318fdd9599599aed5d4c067784a4f9e4cbf0b50b
Andre Martingo <andre.martingo@gmail.com>
2026-06-09 22:13:25 -0700

    Initial Commit

---
+++ privy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  privy
+//
+//  Created by André Martingo on 09/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
